7f60f6dbd41a11b3838c731661b9ab56d92ff570
Julien Moirenc <julien.moirenc@oclock.io>
2021-01-08 11:08:45 +0100

    Version statique finalisée

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -7,13 +7,14 @@ import './converter.scss';
 // == Import local
 import Header from 'src/components/Header';
 import Currencies from 'src/components/Currencies';
+import Result from 'src/components/Result';
 
 // == Composant
 const Converter = () => (
   <div className="converter">
     <Header />
     <Currencies />
-    <div>Result</div>
+    <Result />
   </div>
 );
 
---
+++ src/components/Result/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './result.scss';
+
+const Result = () => (
+  <footer className="result">
+    <h1 className="result__amount">1.09</h1>
+    <h2 className="result__currency">United State Dollar</h2>
+  </footer>
+);
+
+export default Result;
