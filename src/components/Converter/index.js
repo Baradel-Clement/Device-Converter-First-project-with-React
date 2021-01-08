eefd0479a82107fd57cab07ca0570f009266a857
Julien Moirenc <julien.moirenc@oclock.io>
2021-01-08 11:54:04 +0100

    Currencies : gestion des props

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -9,11 +9,13 @@ import Header from 'src/components/Header';
 import Currencies from 'src/components/Currencies';
 import Result from 'src/components/Result';
 
+import currenciesListData from 'src/data/currencies';
+
 // == Composant
 const Converter = () => (
   <div className="converter">
     <Header baseAmount={1} />
-    <Currencies />
+    <Currencies currenciesList={currenciesListData} />
     <Result amount={1.09} currency="USD" />
   </div>
 );
--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './currencies.scss';
 
-const Currencies = () => (
+const Currencies = ({ currenciesList }) => (
   <div className="currencies">
     <h3 className="currencies__title">Currencies</h3>
     <ul className="currencies__list">
-      <li className="currencies__list__item">Australian Dollar</li>
-      <li className="currencies__list__item">Bulgarian Lev</li>
+      {
+        currenciesList.map(
+          (currency) => (
+            <li key={currency.name} className="currencies__list__item">{currency.name}</li>
+          ),
+        )
+      }
     </ul>
   </div>
 );
+
+Currencies.propTypes = {
+  currenciesList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      rate: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
 
 export default Currencies;
