d39fe9e633e3f50e17b5804e8718fcb1da726a84
Julien Moirenc <julien.moirenc@oclock.io>
2021-01-08 10:31:54 +0100

    Composant Header Version Statique : on a vu les variables scss et leur utilisation : @use, vars.-var

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -4,10 +4,15 @@ import React from 'react';
 // == Import
 import './converter.scss';
 
+// == Import local
+import Header from 'src/components/Header';
+
 // == Composant
 const Converter = () => (
   <div className="converter">
-    <h1>Composant : Converter</h1>
+    <Header />
+    <div>Currencies</div>
+    <div>Result</div>
   </div>
 );
 
